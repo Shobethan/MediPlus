08db77cd0e31118d7c09aca1aa1d61453ff2a389
K.Kovarthanan <2011chava@gmail.com>
2018-09-14 14:27:16 +0530

    Showing Diseases
    fetch form firebase

--- src/pages/diseases/diseases.ts
+++ src/pages/diseases/diseases.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController, AlertController } from 'ionic-angular';
 import { DiseaseCreatePage } from '../disease-create/disease-create';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireObject, AngularFireList } from 'angularfire2/database';
+import { database } from 'firebase';
 
 /**
  * Generated class for the DiseasesPage page.
@@ -16,7 +19,25 @@ import { DiseaseCreatePage } from '../disease-create/disease-create';
 })
 export class DiseasesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+
+
+
+  disRef$: AngularFireList<String>;
+  disData: any;
+
+
+
+  
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private afAuth: AngularFireAuth,
+    private afDatabase: AngularFireDatabase,
+    private loadCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController) {
   }
 
   // ionViewDidLoad() {
@@ -26,10 +47,46 @@ export class DiseasesPage {
 
   push__needs_create_page() {
 
-      this.navCtrl.push(DiseaseCreatePage)
+    this.navCtrl.push(DiseaseCreatePage)
 
 
   }
+
+  async ionViewWillEnter() {
+
+    // show the loader
+    var loader = this.loadCtrl.create({
+      spinner: "bubbles",
+      content: "Please wait..."
+    });
+    loader.present();
+
+   
+
+    try {
+      // Get the current userId
+      var userid = this.afAuth.auth.currentUser.uid;
+
+
+      this.disRef$ = this.afDatabase.list<string>(`Disease/${userid}`);
+      this.disData =this.disRef$.valueChanges()
+      loader.dismiss();
+    
+    }
+    catch (e) {
+      
+      let toast = this.toastCtrl.create({
+        message: e,
+        duration: 5000,
+        position: "top",
+        cssClass: "error_toast"
+      });
+      toast.present();
+    }
+  }
+  
+
+  
 
 
 }
